aea72b753e7cb24026f4104fced2a922938bbd07
Alex <alex.formail@gmail.com>
2017-12-24 22:27:31 +0300

    solution is added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  const nd = array.length;
+  const hashMap = new Map();
+  let result = 0;
+
+  for (let i = 0; i < nd; i++) {
+    hashMap.set(array[i], null);
+  }
+
+  for (let [key, value] of hashMap) {
+    if (!hashMap.has(key - 1)) {
+      let t = 1;
+      let j = key + 1;
+
+      while (hashMap.has(j++)) t++;
+
+      if (t > 1 && t > result) {
+        result = t;
+      }
+    }
+  }
+  
+  return result;
 }
